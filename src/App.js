711531335f4c89820fd936e63479d41afd7bb839
oscar <oll301189@gmail.com>
2020-04-20 16:01:12 -0300

    uno

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{Fragment} from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header
+        titulo='Buscar noticias'
+      />
+
+        <div className="container white">
+
+        </div>
+
+    </Fragment>
+  );
+}
+
+export default App;
